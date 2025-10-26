61267f0e9d676af8b6a9f8e35030a49b687e380f
adzidev25 <adzi.fauziahmad@gmail.com>
2025-10-27 03:51:04 +0700

    display delivery method choiced in payment tab

--- app/(main)/checkout/components/shared/AddressCard.tsx
+++ app/(main)/checkout/components/shared/AddressCard.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent } from "@/components/ui/card"
 import { FiEdit } from "react-icons/fi"
 import { useShippingFormStore } from "../../store/checkout.store"
 import { useTabStepStore } from "../../store/tabs.store" 
+import { useEffect } from "react"
 export default function AddressCard() {
 
     const shippingData = useShippingFormStore((state) => state.shippingData)
@@ -13,16 +14,39 @@ export default function AddressCard() {
         useTabStepStore.getState().setActiveTab(1)
     }
 
+    useEffect(() => {
+      
+        console.log("shippingData >>> ", shippingData);
+        
+
+    }, [shippingData])
+    
+
     return (
         <Card className="w-full rounded-md mb-4 py-4">
             <CardContent>
-                <h1 className="text-lg font-semibold mb-2">Shipping Address</h1>
+                <div className="flex items-center justify-between">
+                    <h1 className="text-lg font-semibold mb-2">Shipping Address</h1>
+                    <Button 
+                        type="button" 
+                        variant={"link"} 
+                        onClick={goToShippingForm}
+                        className="text-blue-600 hover:cursor-pointer" >
+                            <FiEdit /> Edit
+                    </Button>
+                </div>
+                
                 <div className="flex items-center justify-between">
                     <p className="text-gray-500">{ `${shippingData?.address} ${shippingData?.city}, ${shippingData?.country} ${shippingData?.zipCode}` }</p>
-                    <Button type="button" variant={"link"} 
-                    onClick={goToShippingForm}
-                     className="text-blue-600 hover:cursor-pointer" ><FiEdit /> Edit Address</Button>
                 </div>
+                <h2 className="font-semibold mt-3 mb-1">Delivery Methods</h2>
+                 <div className="flex items-start">
+                    <div className="flex flex-col gap-1">
+                        <p className="font-medium text-gray-500">$15 - DHL Fast Delivery</p>
+                        <p className="text-sm text-gray-500">Estimated delivery: 3 days</p>
+                    </div>
+                </div>
+                
             </CardContent>
         </Card>
     )
